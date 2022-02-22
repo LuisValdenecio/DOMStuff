0972a171b24150387a0c88f61ee90f50798e0915
Luís Tchitue <ls04af@gmail.com>
2022-02-22 08:18:08 +0000

    adds a basic racing game

---
+++ game.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // the game happens here...
+    const 
+        CAR1 = document.getElementById('car1'), 
+        CAR2 = document.getElementById('car2'),
+        BTN = document.querySelector('button');
+    
+        let INTERVAL1, INTERVAL2;
+
+    BTN.addEventListener('click', function() {
+        startRace();
+    });
+
+    function startRace() {
+        INTERVAL1 = setIntervalHlpr(Math.random() * 1000, CAR1);
+        INTERVAL2 = setIntervalHlpr(Math.random() * 1000, CAR2);
+    }
+
+    function setIntervalHlpr(timeInMls, ele) {
+
+        let stepsForward = 0;
+
+        return setInterval(function() {
+
+            if (stepsForward >= window.innerWidth) {
+                clearInterval(INTERVAL1);
+                clearInterval(INTERVAL2);
+                alert(`${ele.getAttribute("id")} wins!!`);
+                return;
+            }
+
+            ele.style.left = `${stepsForward}px`;
+            stepsForward += window.innerWidth/100;
+        }, timeInMls);
+    }
+
+})
