eaa704cb0e0dc92b4a464c94f382dff5d7f2e038
Luís Tchitue <ls04af@gmail.com>
2022-02-23 17:57:16 +0000

    improves the todo app

--- todo.js
+++ todo.js
@@ -1,36 +1,63 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    const 
-        DATA_BASE = localStorage;
+    const DATA_BASE = localStorage;
 
     let todoList = document.querySelector('ol');
     let noTodoListTitle = document.querySelector('h3');
     let total_of_done_items = document.getElementById('done-items');
     let TOTAL_OF_DONE_ITEMS = 0;
     let inputElement = document.querySelector('input[type="text"]');
-    
-    document.querySelector('form').addEventListener('submit', function(e){
+
+    let removeTodoForm = document.querySelector('form#delete-todo');
+    removeTodoForm.style.display = 'none';
+
+    noTodoListTitle.innerHTML = `The todo items are: ${TOTAL_OF_DONE_ITEMS}`;
+
+    // remove todo
+    removeTodoForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        let indxToDelete = document.querySelector('#index-delete');
+        let liToDelete = todoList.children[indxToDelete.value - 1];
+        todoList.removeChild(liToDelete);
+
+        if (todoList.children.length == 0) {
+            removeTodoForm.style.display = "none";
+        }
+    });
+
+    // add todo
+    document.querySelector('form#add-todo').addEventListener('submit', function(e){
         e.preventDefault(); 
-        noTodoListTitle.innerHTML = 'The todo items are: '
-
-        let liElement = document.createElement('li');
         
-        liElement.innerHTML = inputElement.value;
+        if (inputElement.value != "") {
+            let liElement = document.createElement('li');
+            liElement.innerHTML = inputElement.value;
+            todoList.appendChild(liElement);
+            inputElement.value = "";
 
-        // add the event to the li element  
-        liElement.addEventListener('click', function() {
-            if (liElement.style['text-decoration'] == "line-through") {
-                liElement.style['text-decoration'] = ""
-                TOTAL_OF_DONE_ITEMS--;
-            } else {
-                liElement.style['text-decoration'] = "line-through";
-                TOTAL_OF_DONE_ITEMS++;
+            if (todoList.children.length > 0) {
+                removeTodoForm.style.display = 'block';
+                document.querySelector('.note').innerText = `For example to delete 
+                    \"${todoList.children[0].innerText}\", type in 1. (Make sure the input bellow is selected before hitting enter)`
             }
-            total_of_done_items.innerHTML = `Total done items : ${TOTAL_OF_DONE_ITEMS} / ${todoList.children.length}`;
-        });
 
-        todoList.appendChild(liElement); 
-        inputElement.value = '';
+            // add the event to the li element  
+            liElement.addEventListener('click', function(e) {
+                if (liElement.style['text-decoration'] == "line-through") {
+                    liElement.style['text-decoration'] = "";
+                    TOTAL_OF_DONE_ITEMS--;
+                } else {
+                    liElement.style['text-decoration'] = "line-through";
+                    TOTAL_OF_DONE_ITEMS++;
+                }
+
+                total_of_done_items.innerText = `Finished Items : ${TOTAL_OF_DONE_ITEMS}`;
+            });
+        }   
+
+        noTodoListTitle.innerText = `The todo items are: ${todoList.children.length}`;
+
     });
 
 });
